090be1e0874d3aa004e3e02204d471450716e966
Bryar Osman <bryarosman.bo@gmail.com>
2024-01-20 18:58:32 +0000

    Deploy to heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rzc_qhio^i=0%pbv=e2%7io^w-#%rjnsk3tdt=ah6j9!+a0eng'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=False
 
-ALLOWED_HOSTS = ['8000-bryaro-django-iy53i4iny41.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-bryaro-django-iy53i4iny41.ws-eu107.gitpod.io','.herokuapp.com']
 
 
 # Application definition
