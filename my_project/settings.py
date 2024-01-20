546bef686ba67c1b18e08039a824b7a2187602d1
Bryar Osman <bryarosman.bo@gmail.com>
2024-01-20 19:10:24 +0000

    add DEBUG=True in settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rzc_qhio^i=0%pbv=e2%7io^w-#%rjnsk3tdt=ah6j9!+a0eng'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=False
+DEBUG=True
 
 ALLOWED_HOSTS = ['8000-bryaro-django-iy53i4iny41.ws-eu107.gitpod.io','.herokuapp.com']
 
